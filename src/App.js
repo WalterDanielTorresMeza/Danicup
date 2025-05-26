312bc30b867aa2fd3fec498c15d5f6d445662f98
WalterDanielTorresMeza <38113701+WalterDanielTorresMeza@users.noreply.github.com>
2025-05-26 14:11:51 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
  <p><h1>FECHA Y HORA</h1></p> 
  <p className="l01">Jueves 29 de Mayo | 9:30 am</p>
  <img
-      src="/10.png"
+      src="/la.png"
       alt="Ver en Google Maps"
       className="map-btn"
 />
